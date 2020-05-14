1b54b7b0704891614e4689351aec151c5a37debf
jquinnie <jenn8866@gmail.com>
2020-05-13 19:13:26 -0600

    batch action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 // action creators
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './types';
 // axios instance to make requests over to API
 import streams from '../apis/streams';
 
@@ -18,5 +26,34 @@ export const signOut = () => {
 
 // handle axios creation
 export const createStream = (formValues) => async (dispatch) => {
-  streams.post('/streams', formValues);
+  const response = await streams.post('/streams', formValues);
+
+  // dispatching actionos after stream creation
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+// arrow function that returrns a Thunk function
+export const fetchStreams = async (dispatch) => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  // do not need a response as it will be empty
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
 };
