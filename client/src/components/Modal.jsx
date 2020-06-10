d4504623883919af69a7f58c6960644024870f1e
jquinnie <jenn8866@gmail.com>
2020-06-10 15:03:38 -0600

    Add modal to delete stream and hook up

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// event.stopPropagation will stop event from bubbling up, if onClick does not get handled it will bubble up until it is handled
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className='ui dimmer modals visible active'>
-      <div className='ui standard modal visible active'>some modal stuff</div>
+    <div className="ui dimmer modals visible active" onClick={props.onDismiss}>
+      <div
+        className="ui standard modal visible active"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
     </div>,
     document.querySelector('#modal')
   );
-}
+};
 
 export default Modal;
